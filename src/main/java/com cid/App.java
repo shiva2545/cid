6589ed7bc15654f1781779253e01dae8c5361ce7
root <root@ip-172-31-91-125.ec2.internal>
2019-07-07 16:22:56 +0000

    cid

---
+++ src/main/java/com cid/App.java
@@ -0,0 +1,13 @@
+package com cid;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
